06721000a87cd1dbcd949731826e8c954ebb8883
marielabelcheva <meribelcheva@gmail.com>
2024-12-17 14:13:55 +0200

    Creating Program file

---
+++ GoFish/Program.cpp
@@ -0,0 +1,14 @@
+﻿/*
+*
+* Solution to course project # 8
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2024/2025
+*
+* @author Mariela Belcheva
+* @idnumber 1MI0600413
+* @compiler VC
+*
+* <предназначение на файла>
+*
+*/
