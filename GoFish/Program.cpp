bce96cdcea1ef220add69c307366610063a87e00
marielabelcheva <meribelcheva@gmail.com>
2024-12-24 13:50:14 +0200

    Create and initialize the deck

--- GoFish/Program.cpp
+++ GoFish/Program.cpp
@@ -12,3 +12,38 @@
 * <предназначение на файла>
 *
 */
+
+#include <iostream>;
+
+const short PACKS_IN_THE_DECK = 13;
+const short CARDS_IN_A_PACK = 4;
+
+struct Card
+{
+	char power;
+	unsigned short count;
+};
+
+
+
+int main()
+{
+	Card deck[PACKS_IN_THE_DECK] =
+	{
+		{'2', CARDS_IN_A_PACK},
+		{'3', CARDS_IN_A_PACK},
+		{'4', CARDS_IN_A_PACK},
+		{'5', CARDS_IN_A_PACK},
+		{'6', CARDS_IN_A_PACK},
+		{'7', CARDS_IN_A_PACK},
+		{'8', CARDS_IN_A_PACK},
+		{'9', CARDS_IN_A_PACK},
+		{'10', CARDS_IN_A_PACK},
+		{'J', CARDS_IN_A_PACK},
+		{'Q', CARDS_IN_A_PACK},
+		{'K', CARDS_IN_A_PACK},
+		{'A', CARDS_IN_A_PACK}
+	};
+
+	return 0;
+}
